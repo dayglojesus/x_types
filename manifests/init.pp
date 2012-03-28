03def30f41431cb57dfb185f6515b7ecf21af810
Brian Warsing <bcw@sfu.ca>
2012-03-28 15:23:12 -0700

    add rubycocoa version check to module init

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,13 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class x_types {
-
+    
+  case $::macosx_productversion_major {
+    "10.7": {
+      if $::rubycocoa_version < 1.0.2 {
+        fail("X_types module on Mac OS X Lion requires RubyCocoa 1.0.2 or better. See README for more details.")
+      } # if
+    } # 10.7
+  } # case
 
 }
