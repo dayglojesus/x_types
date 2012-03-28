48eb125880dc08d5a74d979642f8e10f75275ab8
Brian Warsing <bcw@sfu.ca>
2012-03-28 16:29:12 -0700

    conditionals required quoting

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ class x_types {
     
   case $::macosx_productversion_major {
     "10.7": {
-      if $::rubycocoa_version < 1.0.2 {
+      if "$::rubycocoa_version" < "1.0.2" {
         fail("X_types module on Mac OS X Lion requires RubyCocoa 1.0.2 or better. See README for more details.")
       } # if
     } # 10.7
