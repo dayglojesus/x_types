8104753ec808113ffe8c352c3b190eca0deb7bc3
Brian Warsing <bcw@sfu.ca>
2012-03-29 12:40:13 -0700

    gave class a single parameter, $safe added check for new variable

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,14 +11,22 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class x_types {
-    
-  case $::macosx_productversion_major {
-    "10.7": {
-      if "$::rubycocoa_version" < "1.0.2" {
-        fail("X_types module on Mac OS X Lion requires RubyCocoa 1.0.2 or better. See README for more details.")
-      } # if
-    } # 10.7
-  } # case
+class x_types( $safe = 'true' ) {
 
+  if $safe == 'true' {
+    case $::macosx_productversion_major {
+      "10.7": {
+        if "$::rubycocoa_version" < "1.0.2" {
+          fail("X_types module on Mac OS X Lion requires RubyCocoa 1.0.2 or better. See README for more details.")
+        }
+      }
+    }
+  } elsif $safe == 'false' {
+    $msg = "Saftey check disabled. Loading X_types without checking RubyCocoa version."
+    notice($msg)
+    notify { $msg: }
+  } else {
+    fail("Invalid parameter: $safe")
+  }
+  
 }
