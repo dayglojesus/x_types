ce34082d01aecab04ca784767689cd3f311a8a66
Brian Warsing <dayglojesus@gmail.com>
2012-09-14 12:46:27 -0700

    rewrite x_computer to factor out dscl in favor of RubyCocoa

--- lib/puppet/provider/x_computer/x_computer.rb
+++ lib/puppet/provider/x_computer/x_computer.rb
@@ -2,7 +2,6 @@
 # Created: Mon Dec  5 12:19:52 PST 2011
 
 begin
-  require 'pp'
   require 'osx/cocoa'
   include OSX
 rescue LoadError
@@ -10,71 +9,86 @@ rescue LoadError
 end
 
 Puppet::Type.type(:x_computer).provide(:x_computer) do
-  desc "Provides dscl interface for managing Mac OS X computers."
+  desc "Provides RubyCocoa interface for managing Mac OS X computer records with Puppet."
 
-  commands  :dsclcmd          => "/usr/bin/dscl"
-  commands  :uuidgen          => "/usr/bin/uuidgen"
-  confine   :operatingsystem  => :darwin
+  confine     :operatingsystem => :darwin
+  defaultfor  :operatingsystem => :darwin
 
-  @@req_attrib_map_computer = { 'dsAttrTypeStandard:RecordName' => :name,
-    'dsAttrTypeStandard:RealName'     => :name,
-    'dsAttrTypeStandard:ENetAddress'  => :en_address,
-    'dsAttrTypeStandard:HardwareUUID' => :hardware_uuid
-  }
+  @@required_attributes = [ :name, :realname, :en_address, :hardware_uuid ]
 
   def create
+    freshie = @computer.empty?
     info("Creating computer record: #{resource[:name]}")
-    guid = uuidgen.chomp
-    if @computer
-      guid = @computer['dsAttrTypeStandard:GeneratedUID'].to_s
-      raise if guid.nil? or guid.empty?
-      @needs_repair.each do |attrib|
-        dsclcmd "/Local/#{resource[:dslocal_node]}", "-merge", "/Computers/#{resource[:name]}", "#{attrib}", "#{resource[@@req_attrib_map_computer[attrib]]}"
-      end
-    else
-      dsclcmd "/Local/#{resource[:dslocal_node]}", "-create", "/Computers/#{resource[:name]}"
-      @@req_attrib_map_computer.each do |key,value|
-        dsclcmd "/Local/#{resource[:dslocal_node]}", "-merge", "/Computers/#{resource[:name]}", "#{key}", "#{resource[@@req_attrib_map_computer[key]]}"
-      end
+    @@required_attributes.each do |attrib|
+      # info("create: adding #{attrib} attribute, #{resource[attrib]}")
+      @computer[attrib.to_s] = [ resource[attrib] ]
     end
+    @computer['generateduid'] = [ new_generateduid ] unless @computer['generateduid']
+    result = @computer.writeToFile_atomically_(@file, true)
+    raise("Could not write computer plist to file: writeToFile_atomically_ returned nil") if result.nil?
+    # restart_directoryservices(11) if freshie
+    result
   end
 
   def destroy
     info("Destroying computer record: #{resource[:name]}")
-    dsclcmd "/Local/#{resource[:dslocal_node]}", "-delete", "/Computers/#{resource[:name]}"
+    delete_computer
   end
 
   def exists?
-    info("Checking computer record: #{resource[:name]}")
-    # Leopard does nto allow HardwareUUID computer record attribute 
     @kernel_version_major = Facter.kernelmajversion.to_i
-    @@req_attrib_map_computer.delete('dsAttrTypeStandard:HardwareUUID') if @kernel_version_major == 9
-    @needs_repair = []
-    @computer = get_computer(resource[:name])
-    if @computer
-      @@req_attrib_map_computer.each do |key,value|
-        @needs_repair << key unless @computer[key].to_s.eql?(resource[value])
+    @computer = get_computer(resource[:name]) || NSMutableDictionary.new
+    @@required_attributes.delete(:hardware_uuid) if @kernel_version_major == 9
+    info("Checking computer record: #{resource[:name]}")
+    if not @computer.empty?
+      begin
+        return false unless @computer['generateduid']
+        # Roll through each required user attribute to ensure it conforms
+        @@required_attributes.each do |attrib|
+          unless @computer[attrib.to_s].to_ruby.to_s.eql?(resource[attrib])
+            # info("Attrib: #{attrib}, does not match")
+            return false
+          end
+        end
+      rescue
+        info("There was an unspecified error while parsing the computer plist.")
+        return false
       end
-      return false unless @needs_repair.empty?
     else
+      # info("No such record: #{resource[:name]}")
       return false
     end
-    true
   end
 
-  # Returns a hash of computer properties
+  # Return a new generateduid
+  def new_generateduid
+    CFUUIDCreateString(nil, CFUUIDCreate(nil))
+  end
+
+  # Load the computer object
+  ## Returns an NSDictionary representation of the the user.plist if it exists
+  ## If it doesn't, it will return nil
   def get_computer(name)
-    string = `dscl -plist /Local/#{resource[:dslocal_node]} -read /Computers/#{name} 2> /dev/null`.to_ns
-    return false if string.empty? or string.nil?
-    data = string.dataUsingEncoding(OSX::NSUTF8StringEncoding)
-    dict = OSX::NSPropertyListSerialization.objc_send(
-      :propertyListFromData, data,
-      :mutabilityOption, OSX::NSPropertyListMutableContainersAndLeaves,
-      :format, nil,
-      :errorDescription, nil
-    )
-    return false if dict.nil?
-    dict.to_ruby
+    @file = "/private/var/db/dslocal/nodes//#{resource[:dslocal_node]}/computers/#{name}.plist"
+    NSMutableDictionary.dictionaryWithContentsOfFile(@file)
   end
 
+  # Delete the user and applicable shadow hash file
+  def delete_computer
+    begin
+      FileUtils.rm_rf(@file)
+    rescue Puppet::ExecutionFailure => detail
+      fail("Could not destroy the computer record, #{resource[:name]}: #{detail}")
+    end
+  end
+
+  def restart_directoryservices(wait)
+    if @kernel_version_major < 11
+      system('/usr/bin/killall DirectoryService')
+    else
+      system('/usr/bin/killall opendirectoryd')
+    end
+    sleep wait
+  end
+  
 end
